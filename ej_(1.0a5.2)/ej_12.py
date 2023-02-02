8e82f3c17dea277d15fab021a0f709bc3591b73d
47DELtOrZ <deltorz1847@gmail.com>
2023-02-02 11:47:35 +0100

    cod

--- ej_(1.0a5.2)/ej_12.py
+++ ej_(1.0a5.2)/ej_12.py
@@ -2,4 +2,4 @@ num1=int(input("Inserte un numero"))
 num2=int(input("Inserte otro numero"))
 
 op=num1//num2
-print ("ELresultado de la division sin resto es igual a",op)
+print ("El resultado de la division sin resto es igual a",op)
