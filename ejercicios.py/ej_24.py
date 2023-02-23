ef0013d00c07e16e94d2f41661b1f3a18f7c4fbb
47DELtOrZ <deltorz1847@gmail.com>
2023-02-23 11:56:14 +0100

    codedex

---
+++ ejercicios.py/ej_24.py
@@ -0,0 +1,5 @@
+
+num=[10,20,30,40,50]
+
+for i in range(0,2,6):
+    print(num)
---
+++ ejercicios.py/ej_25.py
@@ -0,0 +1,7 @@
+num= []
+for i in range (0,10):
+   num.append(int(input("dame un numero")))
+
+for i in range (0,10):
+        if num[i]%2==0:
+            print(num[i])
